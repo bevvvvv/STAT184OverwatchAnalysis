05eef769628d7b04d950ffc81941696044a68451
bevvvvv <computerwhiz1337@gmail.com>
2018-12-06 12:40:35 -0500

    Ready to scrape profile information

---
+++ battletagScraping.r
@@ -0,0 +1,49 @@
+# Scrape player data from Overwtach
+# https://overwatchtracker.com/leaderboards/pc/global
+# Get battle tags from leaderboards
+
+# Load libaries used
+library(rvest)
+library(data.table)
+library(gdata)
+
+# Read each page's table for battletags
+readTable <- function(url) {
+    table <- url %>%
+        read_html() %>%
+        html_nodes(xpath='/html/body/div[1]/div[1]/div[3]/div[2]/div[2]/div[3]/table') %>%
+        html_table()
+    table <- table[[1]]
+    colnames(table) <- c('Rank', 'Battletag', 'Elo', 'Games')
+    table$Battletag<-trim(table$Battletag)
+    table$pos <- regexpr('\\n', table$Battletag)
+    table$Battletag<-substring(table$Battletag, 0,table$pos-1)
+    table$pos <-NULL
+    table$Rank <-NULL
+    table$Elo<-NULL
+    table$Games<-NULL
+    return(table)
+}
+
+url <- "https://overwatchtracker.com/leaderboards/pc/global"
+
+# See if reading proper html
+# html <- url %>% read_html() %>% html_nodes(xpath='/html/body/div[1]/div[1]/div[3]/div[2]/div[2]/div[3]/table')
+# fileConn<-file("elementHTML.txt")
+# writeLines(as.character(html), con=fileConn, sep="")
+# close(fileConn)
+
+# Read table on current page
+battletags <- readTable(url)[,1]
+# To approximately 950th page
+for (i in 2:950) {
+    url<-'https://overwatchtracker.com/leaderboards/pc/global/CompetitiveRank?page='
+    url<-paste(url,i, sep="")
+    url<-paste(url,'&mode=1', sep="")
+    print(paste('Visiting', url, sep=" "))
+    table <- readTable(url)[,1]
+    print(str(table))
+    battletags <- append(battletags, table)
+}
+# Create a list of just names
+write.csv(battletags, 'battletags.csv', row.names=FALSE)
---
+++ profileScraping.r
@@ -0,0 +1,74 @@
+# Use battletags scraped from leaderboards to get stats from profiles
+
+# Two list - name and value are css classes
+# Variables - names
+# Case - battletag followed by values
+
+
+#empty nodes = skip
+# Load Libraries
+library(rvest)
+library(data.table)
+
+names <-c('Level','Rating','K/D','KDA','Win Pct','Kills/Game','Elim/Game','Damage/Game','Healing/Game',
+            'Elim/Min','Healing/Min','Damage/Min','Solo Kills','Obj kills','Final Blows','Damage Done','Elims','Environment Kills','MultiKills','Deaths')
+xpathValues<-c('/html/body/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[2]/div/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[2]/div/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[1]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[2]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[3]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/div[4]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[5]/div[2]/div[1]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[5]/div[2]/div[2]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[5]/div[2]/div[3]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[5]/div[2]/div[4]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[5]/div[2]/div[5]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[5]/div[2]/div[6]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[1]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[2]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[3]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[4]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[5]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[6]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[6]/div[2]/div[7]/div[2]',
+                '/html/body/div[2]/div[1]/div[3]/div/div[2]/div[7]/div[2]/div[1]/div[2]')
+
+# Function that reads profile, returns default value if no profile found
+addProfile <- function(battletag, table, rowNum) {
+    #url <- 'https://overwatchtracker.com/profile/pc/global/OGE-31607'
+    url <- paste('https://overwatchtracker.com/profile/pc/global/',battletag,sep='')
+    print(rowNum)
+    print(paste('Visiting ', url, sep=' '))
+    result <- tryCatch({
+        download.file(url, destfile = "scrapedpage.html", quiet=TRUE)
+        webpage <- read_html("scrapedpage.html")
+        for(i in 1:length(xpathValues)){
+            table[rowNum, names[i]] <- gsub('\\r|\\n','', webpage %>%
+            html_nodes(xpath=xpathValues[i]) %>%
+            html_text())
+        }
+    }, error = function(cond){
+        table <- table[-rowNum]
+        message(cond)
+    }, warning = function(cond){
+        table <- table[-rowNum]
+        message(cond)
+    })
+    
+    return(table)
+    
+}
+
+profileData <- fread('battletags.csv')
+
+profileData$Battletags <- gsub('#','-',profileData$Battletags)
+end<-length(profileData$Battletags)
+i<-1
+while (i < 100) {
+    profileData<-addProfile(profileData[i,1], profileData, i)
+    if(i%%1000==0) {
+        print(str(profileData))
+    }
+    i<-i+10
+}
+fwrite(profileData,'profileData.csv')
