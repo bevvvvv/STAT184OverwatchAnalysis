9ea491c1953f8e07b0954626e03060bb80ea4e01
bevvvvv <38170041+bevvvvv@users.noreply.github.com>
2018-12-07 13:49:42 -0500

    Update requirements.r

--- requirements.r
+++ requirements.r
@@ -1,3 +1,11 @@
 # Rest of requiresments should be install via the program.
 cranRepo<-"http://cran.rstudio.com/"
 install.packages("optparse", repos=cranRepo)
+install.packages("rvest", repos=cranRepo)
+install.packages("gdata", repos=cranRepo)
+install.packages("ggplot2", repos=cranRepo)
+install.packages("xgboost", repos=cranRepo)
+install.packages("caret", repos=cranRepo)
+install.packages("reshape2", repos=cranRepo)
+install.packages("dplyr", repos=cranRepo)
+install.packages("data.table", repos=cranRepo)
